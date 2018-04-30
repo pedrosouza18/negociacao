87b4a4ee69add9beaff869b067b8b8763410de55
Pedro Souza <souzapedro70@gmail.com>
2018-04-30 19:03:30 -0300

    Criação do projeto

---
+++ aluraframe/client/js/models/Negociacao.js
@@ -0,0 +1,12 @@
+class Negociacao {
+
+  constructor(data, quantidade, valor){
+    this.data = data;
+    this.quantidade = quantidade;
+    this.valor = valor;
+  }
+
+  obtemVolume() {
+    return this.quantidade * this.valor;
+  }
+}
